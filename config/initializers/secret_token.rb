d32c2796f16bff9948e65200e6b684426fa7c0b0
menghy <hanyang.meng@163.com>
2013-03-08 09:48:37 +0800

    new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sms::Application.config.secret_token = '3f25c6d6ccf2d36b0b30701afbe9aad510dcb9f450e96d6a6a91e29cb4ed364a5b7215244ad6c23ef4d293bcbbe72517c3cdfbe78fe78d2102fd3a5cc279db1a'
